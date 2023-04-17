2974cdbe9e85d8fbd0ae203d782f9fa0e4036c16
Aryan Raj <77831132+AryanRajSingh@users.noreply.github.com>
2023-04-17 08:58:05 +0530

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <span>Movie App</span>
+        <span>@2022, Movie, Inc. or its affiliates</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
