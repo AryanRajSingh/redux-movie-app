505da7f5f59f1f5c9ebfc36981b74d236a880aef
Aryan Raj <77831132+AryanRajSingh@users.noreply.github.com>
2023-04-17 09:15:24 +0530

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,1 +1,31 @@
+import './Home.scss';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/moviesSlice/moviesSlice';
 
+import MovieList from '../MovieList/MovieList';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = 'Harry';
+  const showText = 'Sonic';
+
+  useEffect(() => {
+    dispatch(fetchAsyncMovies(movieText));
+    dispatch(fetchAsyncShows(showText));
+  }, [dispatch]);
+
+  return (
+    <section className='home'>
+      <div className='container'>
+        <div className='banner-image'></div>
+        <MovieList />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
