ab8bf8e15b348325aa2745c9fe3eb40d0eaad817
Aryan Raj <77831132+AryanRajSingh@users.noreply.github.com>
2023-04-17 09:04:41 +0530

    Update MovieCard.js

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,1 +1,27 @@
+import { Link } from 'react-router-dom';
+import './MovieCard.scss';
 
+const MovieCard = ({
+  Title: title,
+  Year: year,
+  imdbID: id,
+  Poster: poster,
+}) => {
+  const ghostImage = 'https://via.placeholder.com/300x450';
+
+  return (
+    <Link to={`movie/${id}`}>
+      <div className='movie-card'>
+        <figure>
+          <img src={poster === 'N/A' ? ghostImage : poster} alt={title} />
+        </figure>
+        <div>
+          <h4>{title}</h4>
+          <p>{year}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default MovieCard;
